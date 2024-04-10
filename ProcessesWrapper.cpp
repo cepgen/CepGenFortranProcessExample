6f0e365b5b69de9bdd9d3cd0d4d01540bb410ebf
Laurent Forthomme <laurent.forthomme@cern.ch>
2024-04-10 09:21:41 +0200

    Using new CepGen fortran functions declaration API

--- ProcessesWrapper.cpp
+++ ProcessesWrapper.cpp
@@ -6,18 +6,10 @@
 //=============================================================================
 
 //=============================================================================
-// LIST ALL FORTRAN FUNCTIONS
-// usage:
-//  DECLARE_FORTRAN_FUNCTION( function_name );
-// with the Fortran function name written in lowercase (NO trailing '_')
-//=============================================================================
-
-DECLARE_FORTRAN_FUNCTION(dummy_process);
-
-//=============================================================================
 // MAP name -> Fortran matrix element evaluation function
 // usage:
-//  REGISTER_FORTRAN_PROCESS( name, "description", function_name );
+//  REGISTER_FORTRAN_PROCESS(name, "description", function_name);
+// with the Fortran function name written in lowercase (NO trailing '_')
 //=============================================================================
 
 REGISTER_FORTRAN_PROCESS(dummy, "A dummy Fortran process", dummy_process);
